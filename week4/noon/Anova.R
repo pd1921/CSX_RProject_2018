4632783b338e49e453507a85301925a71f33b3ed
pd1921 <pd1921520@gmail.com>
2018-07-31 20:20:21 +0800

    finish

--- week4/noon/Anova.R
+++ week4/noon/Anova.R
@@ -47,3 +47,49 @@ t.test(Fare ~ Survived, data = full)
 #可加上參數 var.equal=TRUE 來假設變異數同值(不做Welch校正)
 t.test(Fare ~ Survived, data = full, var.equal = TRUE)
 
+library(Hmisc)
+full$Embarked <- factor(full$Embarked, 
+                        levels = c('S',
+                                   'C',
+                                   'Q'
+                        ))
+
+tapply(full$Fare, full$Embarked, mean)
+
+full <- na.omit(full)
+ggplot(data = full, 
+       aes(x = Embarked, y = Fare)) +
+  stat_summary(fun.data = 'mean_cl_boot', size = 1) +
+  scale_y_continuous(breaks = seq(500, 660, by = 20)) +
+  geom_hline(yintercept = mean(full$Fare) , 
+             linetype = 'dotted') +
+  labs(x = '登入港口', y = '票價') +
+  coord_flip()
+
+# Get rid of our missing passenger IDs
+embark_fare <- full %>%
+  filter(PassengerId != 62 & PassengerId != 830)
+
+# Use ggplot2 to visualize embarkment, passenger class, & median fare
+ggplot(embark_fare, aes(x = Embarked, y = Fare, fill = factor(Pclass))) +
+  geom_boxplot() +
+  geom_hline(aes(yintercept=80), 
+             colour='red', linetype='dashed', lwd=2) +
+  scale_y_continuous(labels=dollar_format()) +
+  theme_few()
+
+full$Fsize <- full$SibSp + full$Parch
+anova(m1 <- lm(Fare ~ Fsize, data = full))
+
+#full$Survived <- as.numeric(full$Survived)-1
+ggplot(data = full, 
+       aes(group = Embarked, 
+           y = Fare, x = Pclass)) +
+  geom_point() +
+  stat_smooth(method = 'lm', se = F) +
+  stat_smooth(aes(group = Embarked, 
+                  y = Fare, x = Pclass), 
+              method = 'lm', se = F) + 
+  facet_grid( . ~  Embarked) +
+  labs(x = '港口', y = '票價')
+
